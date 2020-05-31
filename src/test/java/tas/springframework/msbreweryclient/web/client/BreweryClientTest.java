9ead61f46e7aa812ee7c179cef4a7c8b1f9d6cdb
Totte Sjöman <totte.sjoman@gmail.com>
2020-05-31 09:27:01 +0300

    adds tests

---
+++ src/test/java/tas/springframework/msbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,32 @@
+package tas.springframework.msbreweryclient.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tas.springframework.msbreweryclient.web.model.BeerDto;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+}
