5e20eced76e4cb4374246d17ae8668a49d7222c4
Totte Sjöman <totte.sjoman@gmail.com>
2020-06-07 10:12:54 +0300

    adds app props

--- src/main/java/tas/springframework/msbreweryclient/web/config/NIORestTemplateCustomizer.java
+++ src/main/java/tas/springframework/msbreweryclient/web/config/NIORestTemplateCustomizer.java
@@ -6,6 +6,7 @@ import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.nio.reactor.IOReactorException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
@@ -15,16 +16,34 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class NIORestTemplateCustomizer implements RestTemplateCustomizer {
 
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxPerRoute;
+    private final Integer connectTimeout;
+    private final Integer socketTimeout;
+    private final Integer ioThreadCount;
+
+    public NIORestTemplateCustomizer(@Value("${sfg.maxtotalconnections}") Integer maxTotalConnections,
+                                     @Value("${sfg.defaultmaxperroute}") Integer defaultMaxPerRoute,
+                                     @Value("${sfg.connecttimeout}") Integer connectTimeout,
+                                     @Value("${sfg.sockettimeout}") Integer socketTimeout,
+                                     @Value("${sfg.iothreadcount}") Integer ioThreadCount) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxPerRoute = defaultMaxPerRoute;
+        this.connectTimeout = connectTimeout;
+        this.socketTimeout = socketTimeout;
+        this.ioThreadCount = ioThreadCount;
+    }
+
     public ClientHttpRequestFactory clientHttpRequestFactory() throws IOReactorException {
         final DefaultConnectingIOReactor ioReactor = new DefaultConnectingIOReactor(IOReactorConfig.custom()
-                .setConnectTimeout(3000)
-                .setIoThreadCount(4)
-                .setSoTimeout(3000)
+                .setConnectTimeout(connectTimeout)
+                .setIoThreadCount(ioThreadCount)
+                .setSoTimeout(socketTimeout)
                 .build());
 
         final PoolingNHttpClientConnectionManager connectionManager = new PoolingNHttpClientConnectionManager(ioReactor);
-        connectionManager.setMaxTotal(1000);
-        connectionManager.setDefaultMaxPerRoute(100);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
 
         CloseableHttpAsyncClient httpAsyncClient = HttpAsyncClients.custom()
                 .setConnectionManager(connectionManager)
